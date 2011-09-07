aee19080612db7d522c86a13e2192595f45a8bcf
Enrico Maria Casarotti <enricocas@gmail.com>
2011-09-08 01:18:20 +0200

    aggiornamenti db

---
+++ app/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+
+class Coltura(models.Model):
+    id_coltura = models.AutoField(primary_key = True)
+    coltura = models.CharField(max_length = 30)
+    codice = models.PositiveSmallIntegerField(unique =True)
+    def __unicode__(self):
+        return self.name
+
+class Azienda(models.Model):
+    id_azienda = models.AutoField(primary_key = True)
+    nome = models.CharField(max_length = 45)
+    cognome = models.CharField(max_length = 45)
+    indirizzo = models.CharField(max_length =  100)
+    piva =  models.CharField(max_length=18)
+
+    def __unicode__(self):
+        return self.name
+
+class Appezzamento(models.Model):
+    id_appezzamento = models.AutoField(primary_key = True)
+    id = models.CharField(max_length = 4)
+    sau = models.PositiveIntegerField()
+    coltivazione = models.PositiveSmallIntegerField()
+    anno_impianto = models.PositiveSmallIntegerField()
+    ultimo_tratt = models.DateField()
+    coltura = models.ForeignKey(Coltura , to_field = 'codice')
+    ##catasto = models.ManyToManyField(Catasto, through = 'Appezzamenti_catasti')
+
+    def __unicode__(self):
+        return self.name
+
+
+class Proprietario(models.Model):
+    id_proprietario = models.AutoField(primary_key = True)
+    nome = models.CharField(max_length = 45)
+    cognome = models.CharField(max_length = 45)
+
+    def __unicode__(self):
+        return self.name
+
+class Catasto(models.Model):
+    id_catasto = models.AutoField(primary_key = True)
+    provincia = models.PositiveIntegerField()
+    comune = models.PositiveSmallIntegerField()
+    foglio = models.PositiveSmallIntegerField()
+    particella = models.PositiveSmallIntegerField()
+    sub = models.CharField(max_length = 3)
+    sup_tot =  models.PositiveIntegerField()
+    coltura = models.ForeignKey(Coltura, to_field = 'codice')
+    id_proprietario = models.ForeignKey(Proprietario, to_field = 'id_proprietario')
+    azienda = models.ManyToManyField(Azienda, through = 'Appezzamenti_catasti')
+
+    def __unicode__(self):
+        return self.name
+
+
+
+class Appezzamenti_catasti (models.Model):
+    idappezzamenti_catasti = models.AutoField(primary_key = True)
+    id_appezzamento = models.ForeignKey(Appezzamento, to_field = 'id_appezzamento')
+    id_catasto = models.ForeignKey(Catasto, to_field = 'id_catasto')
+    id_azienda = models.ForeignKey(Azienda , to_field = 'id_azienda')
+
+    def __unicode__(self):
+        return self.name
+
--- settings.py
+++ settings.py
@@ -4,15 +4,15 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    # ('Enrico Maria Casarotti', 'enricocas@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db.sql',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -119,6 +119,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'notificabio.app',
 )
 
 # A sample logging configuration. The only tangible logging
